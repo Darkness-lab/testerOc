a8e1b2d36dd5bb381a2f4d401c480633f948219f
Darkness-lab <68563098+Darkness-lab@users.noreply.github.com>
2020-07-20 21:57:53 +0300

    Add files via upload

---
+++ OsTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace OsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   var os = Environment.OSVersion;
+            int mj = os.Version.Major;
+            int mi = os.Version.Minor;
+            if(mj == 6 && mi == 2)
+            {
+                Process.Start("Program.exe");
+            }
+            else
+            {
+                MessageBox.Show("0x8985", "Ошибка", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
